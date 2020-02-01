43d6a76f94ea1ae820aead4fdd89e69c36eb26be
AR123456 <coloradoz99@gmail.com>
2020-02-01 08:07:40 -0700

    update local storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,12 @@ let lastID = parseInt(localStorage.getItem("lastID")) || 0;
 // FUNCTIONS  - reusables
 // ======================
 
-// 4th: function to update localStorage with latest budgetItems and latest lastID
-
+// 4th: function to update localStorage with latest budgetItems and latest lastID  1:35 to 1:38
+// cannot update more than one value at a time  and it has to be a string
+const updateStorage = () => {
+  localStorage.setItem("budgetItems", JSON.stringify(budgetItems));
+  localStorage.setItem("lastID", lastID);
+};
 // 5th: function to render budgetItems on table; each item should be rendered in this format:
 // <tr data-id="2"><td>Oct 14, 2019 5:08 PM</td><td>November Rent</td><td>Rent/Mortgage</td><td>1300</td><td>Fill out lease renewal form!</td><td class="delete"><span>x</span></td></tr>
 // also, update total amount spent on page (based on selected category):
@@ -58,17 +62,11 @@ $("#addItem").on("click", function() {
     alert("something was not filled out ");
     return false;
   }
-
+  // console.log(newItem);
   budgetItems.push(newItem);
   console.log(budgetItems);
   // update local storage & re render budget items
-  // clear out the form 1:29 - 1:35
-  /////// form.reset- a javascript methond not Jquery
-  // document.getElementById("annes-form").reset();
-  ///////// jquery way
-  // $("input, select").val("");
-  // $("#addItemForm input, #addItemForm select").val("");
-  /////// hybrid way - useing jquery to target the form by grabbing element ID and tag name and then doing the reset on the resulting array that jquery creates by resetting the first element in the array
+  // clear out the form 1:29 1:35
   $("#addItemForm form")[0].reset();
 });
 
